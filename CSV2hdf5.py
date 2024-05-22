replace fixed baseline with sample average

CSV2hdf5.py
@@ -16,9 +16,10 @@ if __name__ == '__main__':
 
     NBITS = 14
     LSB_2_VOLT = VMAX / (2**NBITS - 1)
-    BASE_LINE = int(DC_OFFSET*(2**NBITS-1))
-    if POLARITY == -1:
-        BASE_LINE = int((1-DC_OFFSET)*(2**NBITS-1))
+    N_BASELINE_SAMPLES = 8
+    # BASE_LINE = int(DC_OFFSET*(2**NBITS-1))
+    # if POLARITY == -1:
+    #     BASE_LINE = int((1-DC_OFFSET)*(2**NBITS-1))
         
 
     for channel_number in range(5):
@@ -40,7 +41,10 @@ if __name__ == '__main__':
         print("Max time stamp (s): ", time_stamp[-1]/1e9)
 
         samples = raw_data[:,1:]
-        voltagePulses = LSB_2_VOLT * (samples - BASE_LINE) * POLARITY
+        # BASE_LINE = np.mean(samples, axis=1)
+        # baseline is the average of the first 8 samples
+        BASE_LINE = np.mean(samples[:, :N_BASELINE_SAMPLES], axis=1)
+        voltagePulses = LSB_2_VOLT * (samples - BASE_LINE[:, np.newaxis]) * POLARITY
         pulseHeights = np.max(voltagePulses, axis=1)
 
         fig, ax = plt.subplots(1, 1, figsize=(6, 6))

plot_time_series.py
@@ -26,18 +26,28 @@ def create_time_series(pulses, time_stamps):
 
 
 if __name__ == "__main__":
-    coincidence_index = 1
     # Load the data
     with h5py.File("test/coincidences.h5", "r") as f:
         coincident_pulses = f["voltage_pulses"][:]
         time_stamps = f["time_stamps"][:]
 
-    # Plot the data
-    fig, ax = plt.subplots()
-    time_series = create_time_series(coincident_pulses[coincidence_index], time_stamps[coincidence_index])
-    for i in range(5):
-        ax.plot(2*np.arange(time_series.shape[1]), time_series[i], label=f"Channel {i}")
-    ax.set_xlabel("Time (ns)")
-    ax.set_ylabel("Voltage (V)")
-    ax.legend()
-    plt.show()
+    # coincidence_index = 1
+    # # Plot the data
+    # fig, ax = plt.subplots()
+    # time_series = create_time_series(coincident_pulses[coincidence_index], time_stamps[coincidence_index])
+    # for i in range(5):
+    #     ax.plot(2*np.arange(time_series.shape[1]), time_series[i], label=f"Channel {i}")
+    # ax.set_xlabel("Time (ns)")
+    # ax.set_ylabel("Voltage (V)")
+    # ax.legend()
+    # plt.show()
+    for coincidence_index in range(20):
+        # Plot the data
+        fig, ax = plt.subplots()
+        time_series = create_time_series(coincident_pulses[coincidence_index], time_stamps[coincidence_index])
+        for i in range(5):
+            ax.plot(2*np.arange(time_series.shape[1]), time_series[i], label=f"Channel {i}")
+        ax.set_xlabel("Time (ns)")
+        ax.set_ylabel("Voltage (V)")
+        ax.legend()
+        plt.show()